c310f9334a5d316badd467d9f2128b63dc584547
DanyaYashin <yashindan2000@gmail.com>
2020-08-24 18:32:47 +0400

    cocktail menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,14 @@ import Header from "./header"
 export default function Layout(props) {
   return (
     <div>
+    <div className={layoutStyles.topmenu}>
     <Header headerText={props.header} />
     <Link to = '/'><p>Home</p></Link>
     <Link to = '/menu/'><p>Menu</p></Link>
+    </div>
+    <div className={layoutStyles.children}>
+      {props.children}
+    </div>
     </div>
   )
 }
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,13 +2,37 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Header from "../components/header"
+import styles from "./menu.module.css"
+
+
+
+const Cocktail = props => (
+<div className = {styles.cocktail_img}>
+  <div className={styles.block}>
+  <div className = {styles.menu_title_css}> {props.title}</div>
+  <img src = {props.image} className = {styles.menu_img_css}/>
+  <div className = {styles.menu_text_css}> {props.description}</div>
+  </div>
+</div>
+)
 
 export default function Menu() {
   return (
     <Layout header="Menu">
 
-    <div style={{ color: 'teal' }}>
-    </div>
+    <Cocktail
+      title="Пиво"
+      image="https://images.unsplash.com/photo-1523567830207-96731740fa71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+      description ="Просто лучший напиток"
+    />
+
+    <Cocktail
+      title="Отвертка"
+      image="https://images.unsplash.com/photo-1524224313114-ebd9c49dde82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+      description ="..."
+    />
+
+
 
     </Layout>
   )
